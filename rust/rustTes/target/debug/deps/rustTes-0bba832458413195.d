a896c6ca8c95a3a7223a82c2677902a24048cc5d
taylordanger <taylordangersheppard@gmail.com>
2023-05-05 13:23:51 -0500

    RUST_FOR cRUST`

---
+++ rust/rustTes/target/debug/deps/rustTes-0bba832458413195.d
@@ -0,0 +1,5 @@
+/home/darkstar/Desktop/rust/rustTes/target/debug/deps/rustTes-0bba832458413195: src/main.rs
+
+/home/darkstar/Desktop/rust/rustTes/target/debug/deps/rustTes-0bba832458413195.d: src/main.rs
+
+src/main.rs:
